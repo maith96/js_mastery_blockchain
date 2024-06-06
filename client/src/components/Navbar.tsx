897b7dd2f10e69fe1f30ef6217349dc3bb75d238
maith96 <threedaniel385@gmail.com>
2024-06-06 15:36:07 +0300

    create files

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return 
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            
+        </nav>
+
+}
+
+export default Navbar
